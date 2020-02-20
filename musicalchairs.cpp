4357b64c58d6e1882d400547e36442ef8d97a2e6
Govind Balaji S <s.govindbalaji@gmail.com>
2020-02-21 02:26:07 +0530

    Add umpire
    Won't compile without declarations made in player too.

--- musicalchairs.cpp
+++ musicalchairs.cpp
@@ -23,18 +23,33 @@ using namespace std;
 
 bool music_stopped;
 mutex mtx_music_stopped;
+mutex mtx_victim;
 int victim;
 mutex mtx_elimination;
 condition_variable elimination;
 vector<int> free_chairs;
 vector<bool> is_chair_free;
+vector<unsigned long long> sleep_duration;
+vector<mutex> mtx_cv;
 vector<condition_variable> cv;
+mutex mtx_alive_players;
+vector<int> alive_players;
 int nplayers; 
 
+vector<thread> player_threads;
+enum class umpire_state
+  {
+    wait_lapstart,
+    wait_playersleep_musicstart,
+    wait_umpiresleep_musicstop,
+    wait_victim,
+    wait_lapstop,
+    exit
+  };
 
 int main(int argc, char *argv[])
 {
-    int c;
+  int c;
 	nplayers = 0;
 
     /* Loop through each option (and its's arguments) and populate variables */
@@ -105,8 +120,145 @@ void umpire_main()
 {
     /* Add your code here */
 	/* read stdin only from umpire */
+    umpire_state state = wait_lapstart;
+    while (true)
+      {
+        if (state == umpire_state::exit)
+          break;
+        switch (state)
+          {
+          case umpire_state::wait_lapstart:
+            state = waiting_lapstart ();
+            break;
+          
+          case umpire_state::wait_playersleep_musicstart:
+            state = waiting_playersleep_musicstart ();
+            break;
+
+          case umpire_state::wait_umpiresleep_musicstop:
+            state = waiting_umpiresleep_musicstop ();
+            break;
+
+          case umpire_state::wait_victim:
+            state = waitng_victim ();
+            break;
+
+          case umpire_state::wait_lapstop:
+            state = waiting_lapstop ();
+            break;
+      }
 	return;
 }
+
+umpire_state waiting_lapstart ()
+  {
+    string input;
+    cin >> input;
+    if (input != "lap_start")
+      {
+        // handle error
+      }
+    return umpire_state::wait_playersleep_musicstart; 
+  }
+
+umpire_state waiting_playersleep_musicstart ()
+  {
+    string input;
+    while (true)
+      {
+        cin >> input;
+        if (input == "music_start")
+          break;
+        int plid;
+        cin >> plid;
+
+        unique_lock<mutex> lck_cv (mtx_cv[plid]);
+        unique_lock<mutex> lck_sleep (mtx_sleep_duration);
+          cin >> sleep_duration[plid];
+        lck_sleep.unlock ();
+
+        cv[plid].notify_one ();
+        lck_cv.unlock ();
+      }
+    return umpire_state::wait_umpiresleep_musicstop;
+  }
+
+umpire_state waiting_umpiresleep_musicstop ()
+  {
+    string input;
+    while (true)
+      {
+        cin >> input;
+        if (input == "music_stop")
+          break;
+        unsigned long long umpire_sleep_time;
+        cin >> umpire_sleep_time;
+        this_thread::sleep_for (chrono::microseconds (umpire_sleep_time));
+      }
+    unique_lock<mutex> lck_music_stopped (mtx_music_stopped);
+    music_stopped = true;
+    lck_music_stopped.unlock ();
+    for (int plid = 0; plid < nplayers; ++plid)
+      {
+        unique_lock<mutex> lck_cv (mtx_cv[plid]);
+        unique_lock<mutex> lck_sleep (mtx_sleep_duration);
+        sleep_duration[plid] = 0;
+        cv[plid].notify_one ();
+      }
+    return umpire_state::wait_victim;
+  }
+
+umpire_state waiting_victim ()
+  {
+    unique_lock<mutex> lck_elimination (mtx_elimination);
+    elimination.wait (lck_elimination);
+
+    int plid;
+    unique_lock<mutex> lck_victim (mtx_victim);
+    plid = victim;
+    lck_victim.unlock ();
+    player_threads[plid].join ();
+    return umpire_state::wait_lapstop;
+  }
+
+umpire_state waiting_lapstop ()
+  {
+    string input;
+    cin >> input;
+    if (input != "lap_stop")
+      {
+        // handle error
+      }
+
+    unique_lock<mutex> lck_music_stopped (mtx_music_stopped);
+    music_stopped = false;
+    lck_music_stopped.unlock ();
+
+    unique_lock<mutex> lck_alive_players (mtx_alive_players);
+    int lap_no = nplayers - alive_players.size ();
+    lck_alive_players.unlock ();
+
+    cout << "======= lap# " << lap_no << " =======\n";
+
+    unique_lock<mutex> lck_victim (mtx_victim);
+    int plid = victim;
+    lck_victim.unlock ();
+
+    cout << plid << " could not get chair\n**********************";
+
+    lck_alive_players.lock ();
+    if (alive_players.size () == 1)
+      {
+        plid = alive_players[0];
+        cout << "Winner is " << plid << "\n";
+        player_threads[plid].join ();
+        return umpire_state::exit;
+      }
+    else
+      {
+        return umpire_state::wait_lapstart;
+      }
+  }
 
 void player_main(int plid)
 {
