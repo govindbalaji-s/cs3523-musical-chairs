c65fb2cb1c132b68f0af36256007812d61ba29cc
Havya Karuturi <cs18btech11022@iith.ac.in>
2020-02-21 02:56:06 +0530

    Player part
    Not compiling

--- musicalchairs.cpp
+++ musicalchairs.cpp
@@ -21,16 +21,18 @@ unsigned long long musical_chairs();
 
 using namespace std;
 
-bool music_stopped;
+bool music_stopped = false;
 mutex mtx_music_stopped;
+mutex mtx_sleep_duration;
 int victim;
 mutex mtx_elimination;
 condition_variable elimination;
+mutex mtx_chair;
 vector<int> free_chairs;
 vector<bool> is_chair_free;
 vector<condition_variable> cv;
 int nplayers; 
-
+int alive_players;
 
 int main(int argc, char *argv[])
 {
@@ -112,8 +114,92 @@ void player_main(int plid)
 {
     /* Add your code here */
 	/* synchronize stdouts coming from multiple players */
+	
+	while(true)
+	  {
+	  	going_around();
+
+	  	if(hunting_chairs () == -1)
+	  		break;
+	  }
+
+	
 	return;
 }
+
+void going_around()        //waits for sleep or music_stop
+
+{
+	bool mstop;
+
+	do
+	  {
+	    cv[i].wait ();
+
+	    unique_lock<mutex>lck_sleep_duration(mtx_sleep_duration);
+	    unsigned long long slp = sleep_duration[i];
+	    lck_sleep_duration.unlock();
+
+	    if (slp > 0)
+	      {
+            this_thread::sleep_for(chrono::microseconds(slp));  //umpire thread changes the value of sleep[i] to 0
+	      }	
+
+	    unique_lock<mutex>lck_music_stopped(mtx_music_stopped);
+	    mstop = music_stopped;
+	    lck_music_stopped.unlock();
+
+	  }while(!mstop)
+}
+
+int hunting_chairs(int plid)
+{
+	/*function for finding chairs*/
+
+	int fchair;
+
+	while(true)
+	  {
+	    unique_lock<mutex>lck_chair (mtx_chair);
+
+	    if(free_chairs.size () == 0)
+	      {	 
+	      	unique_lock<mutex>lck_alive_players (mtx_alive_players);     	
+	      	alive_players.erase(remove(alive_players.begin(), alive_players.end(), plid), alive_players.end());    //remove from alive players
+	      	lck_alive_players.unlock();
+
+	      	unique_lock<mutex>lck_victim(mtx_victim);
+	      	victim = plid;
+	      	lck_victim.unlock();
+
+	      	unique_lock<mutex>lck_elimination(mtx_elimination)
+	      	elimination.notify_one();
+	      	lck_elimination.unlock();
+
+	      	return -1;
+	      }
+
+		int i = pick_a_chair ();
+		lck_chair.unlock ();
+
+		lck_chair.lock ();
+
+		if (is_chair_free[i])
+		  {
+		  	free_chairs.erase(remove(free_chairs.begin(), free_chairs.end(), i), free_chairs.end());     //remove it from free chairs list
+
+		    is_chair_free[i] = false;
+		    
+		    return i;
+		  }
+		
+		lck_chair.unlock ();
+
+	  } 
+
+
+}
+
 
 unsigned long long musical_chairs()
 {
